5cda2fb71b39fc96b6f137e7f5e2129e4cbce89a
TaYaKi71751 <shantian.tw@gmail.com>
2025-05-22 12:14:26 +0900

    Fix indent

--- src/pages/ListPage.tsx
+++ src/pages/ListPage.tsx
@@ -5,14 +5,15 @@ import { Container, Row } from 'react-bootstrap';
 import BookItem from '../components/BookItem';
 const ListPage: React.FC = () => {
     const [books, setBooks] = useState(bookdata);
-    const [loaded, setLoaded] = useState(false); 
+    const [loaded, setLoaded] = useState(false);
     useEffect(() => {
         if (!loaded) {
             axios.get('https://jamsuham75.github.io/image/data2.json')
                 .then((result) => {
                     console.log(result.data);
-                    const copy = [...books, ...result.data];                     setBooks(copy);         
-                    setLoaded(true);         
+                    const copy = [...books, ...result.data];
+                    setBooks(copy);
+                    setLoaded(true);
                 })
                 .catch(() => {
                     console.log('fail');
@@ -22,20 +23,20 @@ const ListPage: React.FC = () => {
     return (
         <div className="App">
             <Container>
-                <div className="project_header_container">                     
-                    <h1 className="project_header">도서 리스트</h1>                     
+                <div className="project_header_container">
+                    <h1 className="project_header">도서 리스트</h1>
                     <div className="hr"></div>
                 </div>
                 <div>
                     <Row className="text-center">
                         {books.map((item, i) => (
-                            <BookItem key={i} book={item}></BookItem>  
+                            <BookItem key={i} book={item}></BookItem>
                             // key 추가
-                        ))}                     
+                        ))}
                     </Row>
                 </div>
             </Container>
-        </div> 
+        </div>
     );
 };
 export default ListPage;
